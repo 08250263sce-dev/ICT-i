6f1cb343f227764e2b90c03e03df3622f209ecfb
unknown <08250264.sce@rub.edu.bt>
2026-03-20 16:15:28 +0600

    Activity

---
+++ Activity.py
@@ -0,0 +1,15 @@
+print("Hello user")
+Name = input("enter name:")
+num1 = float(input("enter first number:"))
+num2 = float(input("enter second number:"))
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1/num2
+
+print("Hello",Name)
+print("addition:",addition)
+print("subtraction:",subtraction)
+print("multiplication:",multiplication)
+print("division:",division)
+print("Thank you")
